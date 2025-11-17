efd5d4fa716e292f1c11ae987fe94149dbb5fddb
md-alamin2 <mdalamin22671@gmail.com>
2025-11-17 22:14:42 +0600

    learn about destructuring in ts

---
+++ src/destructuring.ts
@@ -0,0 +1,34 @@
+// obj destructuring
+
+const user = {
+    id: 1234,
+    name: {
+        firstName: 'MD.',
+        lastName: 'Al-Amin'
+    },
+    age: 20,
+    married: false,
+    profession: 'Web Developer'
+}
+
+const { id,
+    name: { firstName,
+        lastName }, // nested destructuring
+    age,
+    married: maritalStatus, // name alias 
+    profession } = user;
+
+console.log(`Hello I'm ${firstName + lastName}, I'm ${age} years old. I'am a ${profession}`)
+
+
+
+
+// array destructuring
+
+const friend = ['Samiul', 'Sanower', 'Jamal', 'Kamal', 'Abul', 'Babul'];
+
+const [, // if you want to skip any item
+    secondFriend,
+    thirdFriend] = friend;
+
+console.log(thirdFriend)
